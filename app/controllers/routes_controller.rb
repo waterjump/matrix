1cd780b67125a03607e5c32b8164ca3a987e4e9c
Sean Barrett <sbarrett@Marketings-MacBook-Pro-1.local>
2017-02-07 20:01:44 -0500

    Persist zip files from remote source

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,7 +1,17 @@
 class RoutesController < ApplicationController
 
   def parse
-    @success = 'Success'
-    render json: { status: @success }
+    sources = %w(sentinels sniffers loopholes)
+    sources.each do |src|
+      file_name = "tmp/#{src}.zip"
+      File.delete(file_name)
+      File.open(file_name, 'wb') do |file|
+        file.binmode
+        resp = HTTParty.get("#{Rails.application.secrets.endpoint}?passphrase=#{Rails.application.secrets.passphrase}&source=#{src}")
+        file.write resp.parsed_response
+        file.close
+      end
+    end
+    render json: { status: 'Success' }
   end
 end
--- spec/requests/routes_spec.rb
+++ spec/requests/routes_spec.rb
@@ -2,13 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Routes', type: :request do
   describe 'GET /routes' do
+
+    before(:each) { get routes_path }
+
+    let(:files) do
+      %w(sentinels sniffers loopholes).map do |entry|
+        "#{entry}.zip"
+      end.sort
+    end
+
     it 'works!' do
-      get routes_path
       expect(response).to be_success
       expect(response).to have_http_status(200)
       parsed_body = JSON.parse(response.body)
       expect(parsed_body).to be_kind_of(Hash)
       expect(parsed_body['status']).to eq('Success')
+    end
+
+    it 'saves binary files' do
+      expect((Dir.entries(Rails.root.join('tmp')) & files).sort).to eq(files)
     end
   end
 end
