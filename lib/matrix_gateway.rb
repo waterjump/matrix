9eed17b2d4674f94f69ae390e18230d0878c8337
Sean Barrett <sbarrett@Marketings-MacBook-Pro-1.local>
2017-02-08 20:38:15 -0500

    Tighten up MatrixGateway.

--- lib/matrix_gateway.rb
+++ lib/matrix_gateway.rb
@@ -20,7 +20,12 @@ class MatrixGateway
 
   def call
     Rails.application.config.source_names.each do |src|
-      zip_from_source(src)
+      File.open(file_name(src), 'w+') do |file|
+        file.binmode
+        resp = HTTParty.get("#{self.class.endpoint}&source=#{src}")
+        file.write resp.parsed_response
+        file.close
+      end
     end
   rescue StandardError => error
     notify_error(error)
@@ -39,17 +44,6 @@ class MatrixGateway
     notify_error(error)
   end
 
-  def zip_from_source(src)
-    File.open(file_name(src), 'w+') do |file|
-      file.binmode
-      resp = HTTParty.get("#{self.class.endpoint}&source=#{src}")
-      file.write resp.parsed_response
-      file.close
-    end
-  rescue StandardError => error
-    notify_error(error)
-  end
-
   def file_name(src)
     file_name ||= "tmp/#{src}.zip"
   end
@@ -57,6 +51,6 @@ class MatrixGateway
   def notify_error(error)
     @success = false
     raise error if Rails.env.development? || Rails.env.test?
-    Rails.logger.info(error.message)
+    Rails.logger.info ("ERROR #{[error.message, error.backtrace]}")
   end
 end
